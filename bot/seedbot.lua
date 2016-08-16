9a80bf5f211982229c97642f6500bbfe3a15b16e
NowTeam <aossteam6@gmail.com>
2016-08-16 21:15:58 +0430

    up

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,7 +229,7 @@ function create_config( )
     "joke",
     "logo",
     "Info",
-    "telenow"
+    "telenow",
     },
     sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
